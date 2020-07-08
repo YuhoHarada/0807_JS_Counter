a61f44ad2fc4bfe16fe240276a1383f61b5007bc
YuhoHarada <yuhoharada0616aaa@gmail.com>
2020-07-08 13:05:17 +0200

    ver.1

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+let result = document.getElementById("result")
+function plusOne() {
+    result.innerHTML = Number(result.innerHTML) + 1
+}
+
+function minusOne() {
+    result.innerHTML = Number(result.innerHTML) - 1
+}
+
+function plusTen() {
+    result.innerHTML = Number(result.innerHTML) + 10
+}
+
+function minusTen() {
+    result.innerHTML = Number(result.innerHTML) - 10
+}
+
+function plusHundert() {
+    result.innerHTML = Number(result.innerHTML) + 100
+}
+
+function minusHundert() {
+    result.innerHTML = Number(result.innerHTML) - 100
+}
+
+function Reset() {
+    result.innerHTML = 0
+}
+
+function multipyTwo() {
+    result.innerHTML = Number(result.innerHTML) * 2
+}
